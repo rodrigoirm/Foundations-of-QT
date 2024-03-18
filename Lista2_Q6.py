8d497bcea0a057ccc51795b0fb59980af872e442
Rodrigo Ramos <67575147+rodrigoirm@users.noreply.github.com>
2024-03-18 01:35:11 -0300

    Add files via upload

---
+++ Lista2_Q6.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cvxpy as cp
+import mosek
+
+n=4
+
+A = np.array([[0,0,0,1],[0,0,1,0],[0,1,0,0],[1,0,0,0]])
+B = np.array([[1,0,0,0],[0,-1,0,0],[0,0,-1,0],[0,0,0,1]])
+
+X = cp.Variable((n,n))
+constraints = [cp.trace(B@X)==1,cp.trace(X)==1,X >> 0]
+
+prob = cp.Problem(cp.Minimize(cp.trace(A @ X)),
+                  constraints)
+prob.solve()
+
+# Print result.
+print("The optimal value is", prob.value)
+print("A solution X is")
+print(np.round(X.value,decimals=2))
